5113e729a14ebb27948a8e06ed51d7a3fc3e9d4b
Glen Baker <iepathos@gmail.com>
2015-04-19 16:04:40 -0400

    Code cleanup

---
+++ mojo/static/js/datasync.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){ 
+  if ("WebSocket" in window) {
+    var ws = new WebSocket("ws://localhost:8888/datasync/");
+    
+    ws.onopen = function() {
+      console.log('Seadog DataSync online');
+    };
+    
+    ws.onmessage = function (evt) {
+      
+      // WebSocket DataSync to UI
+      var json = evt.data;
+      msg = JSON.parse(json);
+      
+
+      if (msg['funds'] != null) {
+        $('#wallet-funds').html(msg['funds']);
+      }
+
+
+
+    };
+    
+    ws.onclose = function() {
+      console.log('Seadog DataSync offline');
+    };
+  } else {
+    alert("WebSocket not supported");
+  }
+});
