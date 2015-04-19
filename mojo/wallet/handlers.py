6a54704732a27f962ec726c774aca84c2c1ba178
Glen Baker <iepathos@gmail.com>
2015-04-19 16:11:36 -0400

    Code cleanup

--- mojo/wallet/handlers.py
+++ mojo/wallet/handlers.py
@@ -3,7 +3,7 @@
 from tornado.gen import coroutine
 from mojo.util import template
 from mojo.handlers import BaseHandler
-from mojo.wallet.wallet import deposit, get_funds
+from mojo.wallet.wallet import deposit
 from tornado.web import authenticated
 
 
@@ -18,17 +18,3 @@ class WalletHandler(BaseHandler):
     def post(self):
         yield deposit(self.db, self.get_current_user(), 100)
         self.redirect('/')
-
-
-# class WalletFundsHandler(BaseHandler):
-
-#     @authenticated
-#     @coroutine
-#     def post(self):
-#         user = self.get_current_user()
-#         # funds = yield get_funds(self.db, user)
-#         feed = yield self.users.get(user).changes().run(self.db)
-#         while (yield field.fetch_next()):
-#             change = yield feed.next()
-#             funds = change['new_val']['funds']
-#             self.write({'funds': funds})
